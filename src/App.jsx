032f8a67b3b50edd822190f9ee2215585dc39979
Abdeali Jaroli <56578305+abdealijaroli@users.noreply.github.com>
2023-02-10 14:10:02 +0530

    let's see what Page.jsx has to offer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
-// import Page from './components/Page'
+import Page from './components/Page'
 
 function App() {
   return (
-    <Home />
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/myLinux" element={<Page />} />
-    //   </Routes>
-    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/myLinux" element={<Page />} />
+      </Routes>
+    </Router>
   )
 }
 
